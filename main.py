88d775c150592a698be4d74c2d8b536447d56803
Om Amin <omamin135@gmail.com>
2023-02-28 17:07:30 -0500

    added command line support to get paths to folders

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
-import os
-import re
-
-C_FILES = "c_files/"
-HEADER_FILES = "header_files/"
+import os 
+import re #import regex
+import sys
 
 
+# get the path to the folders containing the c files and/or header files
+# paths given by command line arguments, otherwise default set to directory of main.py file
+def getFolderPaths():
+
+    #By default get .c files and export .h files into directory of main.py file
+    cFolder = os.getcwd()
+    hFolder = os.getcwd()
+
+    # if .c folder argument given, set it
+    if (len(sys.argv) == 2):
+        cFolder = sys.argv[1]
+    
+    # if both .c and .h argument given, set it
+    if (len(sys.argv) > 2):
+        cFolder = sys.argv[1]
+        hFolder = sys.argv[2]
+    
+    return (cFolder, hFolder)
+
+# given a list of lines, returns list of lines of function protypes
 def parseFile(lines):
     protos = []
 
     for line in lines:
+        #match when line starts with keyword
         match = re.match("(\w+ ).*", line)
         if match != None:
+            #filter out the non function protype keywords
             if (not exclude(match[0])):
                 protos.append(match[0])
 
     return protos
 
+#returns true if the line begins with invalid keyword
 def exclude(line):
     exclude = {
         "static", 
         "struct"
     }
 
+    #grab first keyword
     keyword = re.search("\w+", line, flags=0)[0]
     
     return (keyword in exclude)
 
+#creates header file and print all function protypes to it
 def constructFile(protos, fileName):
-    path = HEADER_FILES + fileName + ".h"
+    #set path to header file
+    path = HEADER_FILES + fileName[:-2:] + ".h"
     file = open(path, "w")
 
+    #write all the function prototypes
     for p in protos:
-        
         p = prepProto(p)
         file.write(p + "\n")
 
     file.close()
 
+# removes and trailing '{' and add ';' to end of prototype
 def prepProto(proto):
     #if trailing {, remove it
     if (proto[-1] == '{'):
@@ -46,18 +71,21 @@ def prepProto(proto):
     if (proto[-1] != ';'):
         proto += ';'
         
-    
     return proto
 
-    
+#get the paths to the .c and .h files
+C_FILES, HEADER_FILES = getFolderPaths()
 
-
+#os.walk to iterate through all subfolder aswell
 for root, dirs, files in os.walk(C_FILES):
     for filename in files:
-        with open(os.path.join(root, filename)) as fp:
-            lines = fp.readlines()
-            protos  = parseFile(lines)
-            constructFile(protos, filename)
+        #for any file with .c extension
+        if(filename[len(filename)-2:] == ".c"):
+            with open(os.path.join(root, filename)) as fp:
+                #grab all the lines and create header files
+                lines = fp.readlines()
+                protos  = parseFile(lines)
+                constructFile(protos, filename)
 
             
         
